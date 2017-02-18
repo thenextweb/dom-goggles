c8ac900b91fecaaa1ab5466a340de829111bbde6
Laura González <lawr@lauragonzalez.cc>
2017-02-18 18:20:02 +0100

    Initial

---
+++ main.js
@@ -0,0 +1,104 @@
+const Zombie = require('zombie');
+
+const test = (url,tests) => {
+	const browser = new Zombie();
+	let solution = {};
+	return new Promise((resolve, reject)=>{
+		browser.fetch(url)
+			.then(function(response) {
+				if (response.status === 200) {
+					return response.text();
+				}
+				else {
+					reject(response)
+				}
+			})
+			.then(function(text) {
+				tests.map(test => {
+
+					let regex = (!test.test)?
+						new RegExp(test,'g'):
+						test
+
+					try {
+						solution[test] = text.match(regex).length
+					} catch(e) {
+						solution[test] = 0;
+					}
+				})
+				resolve(solution)
+			})
+			.catch(reject);
+	});
+}
+
+const main = (webpages, tests, options={}) => {
+
+	if(typeof webpages === 'string') {
+		webpages = [webpages]
+	}
+	if(typeof tests === 'string') {
+		tests = [tests]
+	}
+
+	if(!tests || !webpages) {
+		throw 'Things are missing!';
+	}
+
+	return new Promise((resolve, reject)=>{
+
+		let resultsTotal = {};
+		let resultsByWebpage = {};
+		let count = 0;
+
+		const finalPass = () => {
+			Object.keys(resultsByWebpage).map(url => {
+				Object.keys(resultsByWebpage[url]).map(test => {
+					if(!resultsTotal[test]) resultsTotal[test] = 0;
+					if(resultsByWebpage[url][test] > 0) {
+						resultsTotal[test] += resultsByWebpage[url][test];
+					}
+				})
+			})
+			resolve({
+				total: resultsTotal,
+				byWebpage: resultsByWebpage
+			});
+		}
+
+		const returnMaybe = () => {
+			count++;
+			if(count >= webpages.length) {
+				finalPass()
+			}
+		}
+
+		if(options.report) console.log('> Started');
+
+		webpages.map(url => {
+			test(url,tests)
+			.then(solution => {
+				if(options.report) {
+					let results = (()=>{
+						let arr = [];
+						Object.keys(solution).map(key=>{
+							if(solution[key] > 0) arr.push(key)
+						});
+						return arr;
+					})();
+					console.log(results);
+					console.log(`> ${url} – ${results.length}/${tests.length} `);
+				}
+				resultsByWebpage[url] = solution
+				returnMaybe();
+			})
+			.catch(error => {
+				reject(error);
+			})
+		});
+
+	});
+};
+
+
+module.exports = main;
---
+++ test/fetch.js
@@ -0,0 +1,102 @@
+const expect = require("chai").expect;
+const domGoggles = require('../main');
+
+describe("Dom goggles", function() {
+
+
+	it("looks for a body tag on google.com", function(done) {
+
+		domGoggles(
+			'http://google.com',
+			'<body'
+		).then(results => {
+			try {
+				expect(results.total['<body']).to.be.at.least(1)
+				done();
+			} catch(e) {
+				done(e);
+			}
+		}).catch(e => {
+			done(e)
+		})
+
+	});
+
+
+	it("looks for a body, <div> tag on google.com & facebook", function(done) {
+
+		domGoggles(
+			['http://google.com','http://facebook.com'],
+			['<body','<div']
+		).then(results => {
+			try {
+				expect(results.total['<body']).to.be.at.least(2)
+				done();
+			} catch(e) {
+				done(e);
+			}
+		}).catch(e => {
+			done(e)
+		})
+
+	});
+
+
+	it("looks for a body, <div> tag on google.com & facebook using a regex", function(done) {
+
+		domGoggles(
+			['http://google.com','http://facebook.com'],
+			[/<body/,/<div/]
+		).then(results => {
+			try {
+				expect(results.total[/<body/]).to.be.at.least(2)
+				done();
+			} catch(e) {
+				done(e);
+			}
+		}).catch(e => {
+			done(e)
+		})
+
+	});
+
+
+	it("looks for more than 3 divs on facebook.com", function(done) {
+
+		domGoggles(
+			['http://facebook.com'],
+			['<div']
+		).then(results => {
+			try {
+				expect(results.total['<div']).to.be.above(4)
+				done();
+			} catch(e) {
+				done(e);
+			}
+		}).catch(e => {
+			done(e)
+		})
+
+	});
+
+
+	it("looks for a non existent (HOPEFULLY) <wrigglityboo> tag on google.com & facebook", function(done) {
+
+		domGoggles(
+			['http://google.com','http://facebook.com'],
+			['<wrigglityboo']
+		).then(results => {
+			try {
+				expect(results.total['<wrigglityboo']).to.equal(0)
+				done();
+			} catch(e) {
+				done(e);
+			}
+		}).catch(e => {
+			done(e)
+		})
+
+	});
+
+
+});
